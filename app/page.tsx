27423271e61a787c2c7d10c7a08dc0fe036cd279
Kurian Sybu Cherian <117100527+Kurian7007@users.noreply.github.com>
2025-01-14 13:56:36 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,45 +12,52 @@ export default function App() {
   const cards = [
     {
       image: "/cardgrid/slide1.jpg",
-      title: "EXTRUSION DIVISION",
+      title: "EXTRUSION DIVISION",
       description: "76,800 metric ton",
       unit: "Output per annum",
+      link: "/about/ourgroups/extrudedprofiles", // Add a link for each card
     },
     {
       image: "/cardgrid/slide2.jpg",
-      title: "POWDER COATING DIVISION",
+      title: "POWDER COATING DIVISION",
       description: "44,400 metric ton",
       unit: "Output per annum",
+      link: "/about/ourgroups/extrudedprofiles",
     },
     {
       image: "/cardgrid/slide3.jpg",
-      title: "ANODIZING DIVISION",
+      title: "ANODIZING DIVISION",
       description: "14,800 metric ton",
       unit: "Output per annum",
+      link: "/about/ourgroups/extrudedprofiles",
     },
     {
       image: "/cardgrid/slide4.jpg",
-      title: "WOOD COATING DIVISION",
+      title: "WOOD COATING DIVISION",
       description: "1500 metric ton",
       unit: "Output per annum",
+      link: "/wood-coating-division",
     },
     {
       image: "/cardgrid/slide5.jpg",
-      title: "EXTRUSION DIE DIVISION",
+      title: "EXTRUSION DIE DIVISION",
       description: "1800 Dies",
       unit: "Output per annum",
+      link: "/about/ourgroups/extrudedprofiles",
     },
     {
       image: "/cardgrid/slide6.jpg",
-      title: "EPDM RUBBER PRODUCTS DIVISION",
+      title: "EPDM RUBBER PRODUCTS DIVISION",
       description: "1800 metric ton",
       unit: "Output per annum",
+      link: "/epdm-rubber-products-division",
     },
     {
       image: "/cardgrid/slide7.jpg",
-      title: "ALUMINIUM COIL & SHEET DIVISION",
+      title: "ALUMINIUM COIL & SHEET DIVISION",
       description: "30,000 metric ton",
       unit: "Output per annum",
+      link: "/aluminium-coil-sheet-division",
     },
   ];
 
