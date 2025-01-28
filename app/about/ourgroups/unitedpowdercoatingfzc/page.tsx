3640fba2b7362ea523d040f60cafb50e5d02fcc5
Kurian Sybu Cherian <117100527+Kurian7007@users.noreply.github.com>
2025-01-28 08:55:04 +0400

    mobile view done

--- app/about/ourgroups/unitedpowdercoatingfzc/page.tsx
+++ app/about/ourgroups/unitedpowdercoatingfzc/page.tsx
@@ -69,12 +69,12 @@ function page() {
         <div>
           {/* Top Section */}
           <motion.div
-            className="grid grid-cols-12 max-w-7xl mx-auto p-6 items-center gap-y-8 min-h-screen"
+            className="grid grid-cols-12 max-w-7xl mx-auto p-6 items-center gap-y-8"
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 1.2, ease: "easeOut" }}
-            style={{ marginTop: "-150px" }} // Added negative margin to shift it upwards
+             // Added negative margin to shift it upwards
           >
             {/* Text Column */}
             <div className="col-span-12 md:col-span-7">
@@ -139,7 +139,7 @@ function page() {
             whileInView={{ x: 0, opacity: 1 }}
             viewport={{ once: true }}
             transition={{ duration: 2.0 }}
-            style={{ marginTop: "-180px" }}
+            
           >
             <hr className="w-2 h-16 md:h-24 bg-[#E8D858] border-none" />
             <h2 className="text-3xl md:text-5xl font-semibold text-[#023D68] leading-tight mb-3">
@@ -224,4 +224,3 @@ function page() {
 }
 
 export default page;
-
