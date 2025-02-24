0eb44ae5768fc48f3c671d0b611609ac33485864
Kurian Sybu Cherian <117100527+Kurian7007@users.noreply.github.com>
2025-02-24 10:20:19 +0400

    Update page.tsx

--- app/about/oursuppliers/page.tsx
+++ app/about/oursuppliers/page.tsx
@@ -52,7 +52,12 @@ const data = [
 
 function page() {
   return (
-    <div>
+    <motion.div
+      className="page-container"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: "easeOut" }} // Smooth fade-in with a vertical motion
+    >
       <Header />
       {/* Banner Section */}
       <div
@@ -145,7 +150,7 @@ function page() {
       ))}
 
       <Footer />
-    </div>
+    </motion.div>
   );
 }
 
